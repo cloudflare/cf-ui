fbc3745268f7ea464385b34983b3e6d3f009f560
James Kyle <me@thejameskyle.com>
2016-02-08 15:10:24 -0800

    Use watchify for examples

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -1,25 +1,21 @@
 #!/bin/sh
 set -e
 
-echo "Building repos..."
 ./node_modules/.bin/gulp build
 
-echo "Building vendor.js..."
-./node_modules/.bin/browserify \
+./node_modules/.bin/watchify \
+  -v \
   -r react \
   -r react-dom \
   -r lodash --noparse lodash \
   -r react-addons-css-transition-group \
-  -o example/vendor.js
-
-echo "Building bundle.js..."
-./node_modules/.bin/browserify packages/cf-component-*/example/*/component.js \
+  -o example/vendor.js & \
+./node_modules/.bin/watchify packages/cf-component-*/example/*/component.js \
+  -v \
   -x react \
   -x react-dom \
   -x lodash \
   -x react-addons-css-transition-group \
   -t babelify \
-  -o example/bundle.js
-
-echo "Starting server..."
+  -o example/bundle.js & \
 node ./example/server.js
