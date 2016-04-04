b3814d1f3b087fb250f319b2a4e8f6b9a2a5ae39
James Kyle <me@thejameskyle.com>
2016-04-04 15:25:41 -0700

    Rewrite build system for static site

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,51 @@
-const gulp = require('gulp');
-const gutil = require('gulp-util');
+const combineStreams = require('stream-combiner');
+const sourcemaps = require('gulp-sourcemaps');
+const Highlights = require('highlights');
+const browserify = require('browserify');
+const pathExists = require('path-exists');
+const watchify = require('watchify');
+const babelify = require('babelify');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
 const through = require('through2');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const eslint = require('gulp-eslint');
+const uglify = require('gulp-uglify');
+const mkdirp = require('mkdirp');
+const child = require('child_process');
+const gutil = require('gulp-util');
 const chalk = require('chalk');
 const babel = require('gulp-babel');
-const path = require('path');
-const plumber = require('gulp-plumber');
 const newer = require('gulp-newer');
 const watch = require('gulp-watch');
-const eslint = require('gulp-eslint');
 const karma = require('karma');
+const path = require('path');
+const gulp = require('gulp');
+const glob = require('glob');
+const del = require('del');
+const fs = require('fs');
+const _ = require('lodash');
+
+const highlighter = new Highlights();
+
+highlighter.requireGrammarsSync({
+  modulePath: require.resolve('language-babel/package.json')
+});
 
 const scripts = './packages/*/src/**/*.js';
 const dest = 'packages';
+
+const exampleComponents = 'packages/cf-component-*/example/*/component.js';
+const exampleStyles = ['example/base.css', 'example/styles.css'];
+const exampleIcons = 'example/icons/*';
+
+const vendors = [
+  'react',
+  'react-dom',
+  'lodash',
+  'react-addons-css-transition-group'
+];
 
 let srcEx, libFragment;
 
@@ -22,6 +56,12 @@ if (path.win32 === path) {
   srcEx = new RegExp('(packages/[^/]+)/src/');
   libFragment = '$1/lib/';
 }
+
+function execSync(cmd) {
+  return child.execSync(cmd, {
+    encoding: 'utf8'
+  }).trim();
+};
 
 function fixErrorHandling() {
   return plumber({
@@ -50,11 +90,11 @@ function initKarmaServer(singleRun, cb) {
   }, cb).start();
 }
 
-function build(dev) {
+function compile(watch) {
   return gulp.src(scripts)
     .pipe(fixErrorHandling())
     .pipe(renameSrcToLib())
-    .pipe(dev ? newer(dest) : gutil.noop())
+    .pipe(watch ? newer(dest) : gutil.noop())
     .pipe(through.obj((file, enc, callback) => {
       const filepath = path.relative(path.resolve(__dirname, 'packages'), file._path);
       gutil.log('Compiling', '\'' + chalk.cyan(filepath) + '\'...');
@@ -62,39 +102,228 @@ function build(dev) {
     }))
     .pipe(babel())
     .pipe(gulp.dest(dest));
+};
+
+function initBrowserify(files, output, externals, requires, transforms, watch) {
+  const options = {
+    transform: transforms
+  };
+
+  if (watch) {
+    _.assign(options, watchify.args);
+  }
+
+  let bundler = browserify(files, options);
+
+  if (externals) {
+    bundler.external(externals);
+  }
+
+  if (requires) {
+    bundler.require(requires);
+  }
+
+  if (watch) {
+    bundler = watchify(bundler);
+    bundler.on('update', update);
+    bundler.on('log', gutil.log);
+    bundler.on('update', () => gutil.log('Rebundling...'));
+    bundler.on('time', time => {
+      gutil.log('Rebundled in:', gutil.colors.cyan(time + 'ms'))
+    });
+  }
+
+  function update() {
+    return bundler.bundle()
+      .on('error', err => {
+        gutil.log(err);
+        this.emit('end');
+      })
+      .pipe(source(output))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({
+        loadMaps: true
+      }))
+      .pipe(watch ? gutil.noop() : uglify())
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('dist'));
+  }
+
+  return update();
 }
 
-function lint() {
-  gulp.src('./packages/*/src/**/*.js')
+function initBrowserifyBundle(watch) {
+  const files = glob.sync(path.resolve(__dirname, exampleComponents));
+  return initBrowserify(files, 'bundle.js', vendors, null, [babelify], watch);
+}
+
+function initBrowserifyVendor(watch) {
+  return initBrowserify([], 'vendor.js', null, vendors, [], watch);
+}
+
+export function examplesBuildHtml(cb) {
+  const packagesPath = path.resolve(__dirname, 'packages');
+  const packages = fs.readdirSync(packagesPath).filter(pkg => {
+    return pkg.indexOf('cf-component-') > -1;
+  });
+
+  let componentsHtml = '';
+  let sidebarHtml = '';
+
+  packages.forEach(pkg => {
+    const examplesPath = path.resolve(packagesPath, pkg, 'example');
+
+    if (!pathExists.sync(examplesPath)) {
+      return;
+    }
+
+    const examples = fs.readdirSync(examplesPath);
+
+    componentsHtml += '<h2 class="cf-example__heading" id="' + pkg + '">' + pkg + '</h2>';
+
+    examples.forEach(example => {
+      const contentPath = path.join(examplesPath, example, 'component.js');
+      const content = fs.readFileSync(contentPath).toString()
+
+      const higlightedContent = highlighter.highlightSync({
+        fileContents: content,
+        filePath: contentPath,
+        scopeName: 'source.js'
+      });
+
+      if (example !== 'basic') {
+        componentsHtml += '<p>' + example + '</p>';
+      }
+
+      componentsHtml += (
+        '<div class="cf-example">' +
+        '<div class="cf-example__content" id="' + pkg + '--' + example + '"></div>' +
+        '<div class="cf-example__code">' + higlightedContent + '</div>' +
+        '</div>'
+      );
+    });
+
+    sidebarHtml += '<a href="#' + pkg + '">' + pkg + '</a>';
+  });
+
+  const styles = gutil.env.public ? 'styles.css' : '../../www-style/htdocs/static/stylesheets/components.css';
+
+  const html = (
+    '<!doctype html>' +
+    '<html>' +
+    '<head>' +
+    '<meta name="viewport" content="width=device-width">' +
+    '<title>CloudFlare Components</title>' +
+    '<link rel="stylesheet" href="base.css">' +
+    '<link rel="stylesheet" href="' + styles + '">' +
+    '</head>' +
+    '<body>' +
+    '<header class="cf-example-header">cf-ui</header>' +
+    '<nav class="cf-example-sidebar">' + sidebarHtml + '</nav>' +
+    '<div class="cf-example-content">' + componentsHtml + '</div>' +
+    '<script type="text/javascript" src="vendor.js"></script>' +
+    '<script type="text/javascript" src="bundle.js"></script>' +
+    '</body>' +
+    '</html>'
+  );
+
+  mkdirp.sync(path.resolve(__dirname, 'dist'));
+  fs.writeFileSync(path.resolve(__dirname, 'dist/index.html'), html);
+  cb();
+}
+
+export const build = () => compile(false);
+
+export function lint() {
+  return gulp.src('./packages/*/src/**/*.js')
     .pipe(fixErrorHandling())
     .pipe(eslint())
     .pipe(eslint.format())
-
     // for some reason eslint doesn't work without this
     .on('data', file => {});
-}
+};
 
-function test(cb) {
-  initKarmaServer(true, cb);
-}
+export const test = cb => initKarmaServer(true, cb);
+const testWatch = cb => initKarmaServer(false, cb);
 
-function testWatch(cb) {
-  initKarmaServer(false, cb);
-}
-
-function dev(cb) {
+export function dev() {
   watch(scripts, () => {
-    build(true);
+    compile(true);
     lint();
   });
   testWatch(cb);
+};
+
+export function examplesBuildCss() {
+  return gulp.src(exampleStyles)
+    .pipe(sourcemaps.init())
+    .pipe(postcss([
+      require('autoprefixer'),
+      require('cssnano')()
+    ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
 }
 
-gulp.task('build', function() {
-  return build(false);
-});
+export function examplesBuildIcons() {
+  return gulp.src(exampleIcons)
+    .pipe(gulp.dest('dist/icons'));
+}
 
-gulp.task('lint', lint);
-gulp.task('test', test);
+export const examplesClean = () => del(['dist']);
 
-gulp.task('dev', ['build'], dev);
+export const examplesBuildBundle = () => initBrowserifyBundle(false);
+export const examplesBuildVendor = () => initBrowserifyVendor(false);
+export const examplesDevBundle = () => initBrowserifyBundle(true);
+export const examplesDevVendor = () => initBrowserifyVendor(true);
+
+export const examplesBuild =
+  gulp.series(
+    examplesClean,
+    gulp.parallel(
+      examplesBuildHtml,
+      examplesBuildCss,
+      examplesBuildIcons,
+      examplesBuildBundle,
+      examplesBuildVendor
+    )
+  );
+
+export const examplesDev =
+  gulp.series(
+    examplesClean,
+    gulp.parallel(
+      examplesBuildHtml,
+      examplesBuildCss,
+      examplesBuildIcons,
+      examplesDevBundle,
+      examplesDevVendor
+    ),
+    function examplesWatcher() {
+      gulp.watch(exampleComponents, examplesBuildHtml);
+      gulp.watch(exampleStyles, examplesBuildCss);
+      gulp.watch(exampleIcons, examplesBuildIcons);
+    }
+  );
+
+function setEnvPublic(cb) {
+  gutil.env.public = true;
+  cb();
+}
+
+export const examplesPublish =
+  gulp.series(
+    setEnvPublic,
+    examplesBuild,
+    function examplesPublisher(cb) {
+      execSync([
+        'cd dist',
+        'git init',
+        'git remote add origin git@github.com:cloudflare/cf-ui.git',
+        'git add -A',
+        'git commit -m "[Deploy]"',
+        'git push origin master:gh-pages --force'
+      ].join(' && '));
+      cb();
+    }
+  );
