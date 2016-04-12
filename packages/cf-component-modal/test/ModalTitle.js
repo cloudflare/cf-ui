e55e255b9da8cb4b42fd0572c2eab7f49f3df718
James Kyle <me@thejameskyle.com>
2016-04-12 13:15:31 -0700

    Use cf-component-heading in ModalTitle

--- packages/cf-component-modal/test/ModalTitle.js
+++ packages/cf-component-modal/test/ModalTitle.js
@@ -1,13 +1,16 @@
 const React = require('react');
 const assertEqualJSX = require('../../../utils/assertEqualJSX');
 const ModalTitle = require('../src/ModalTitle');
+const {Heading} = require('cf-component-heading');
 
 describe('ModalTitle', function() {
   it('should render', function() {
     assertEqualJSX(
       <ModalTitle>ModalTitle</ModalTitle>,
       // should equal
-      <h4 className="cf-modal__title">ModalTitle</h4>
+      <div className="cf-modal__title">
+        <Heading size={3}>ModalTitle</Heading>
+      </div>
     );
   });
 });
