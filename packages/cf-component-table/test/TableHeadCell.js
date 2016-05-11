f8e632ccaacf5c4322878e0d949f8a382a47af2d
James Kyle <me@thejameskyle.com>
2016-05-11 13:52:57 -0700

    Fix cf-component-table tests

--- packages/cf-component-table/test/TableHeadCell.js
+++ packages/cf-component-table/test/TableHeadCell.js
@@ -10,20 +10,4 @@ describe('TableHeadCell', function() {
       <th className="cf-table__cell cf-table__cell--head">TableHeadCell</th>
     );
   });
-
-  it('should render with sortable', function() {
-    assertEqualJSX(
-      <TableHeadCell sortable>TableHeadCell</TableHeadCell>,
-      // should equal
-      <th className="cf-table__cell cf-table__cell--head cf-table__cell--sortable">TableHeadCell</th>
-    );
-  });
-
-  it('should render with editable', function() {
-    assertEqualJSX(
-      <TableHeadCell editable>TableHeadCell</TableHeadCell>,
-      // should equal
-      <th className="cf-table__cell cf-table__cell--head cf-table__cell--editable">TableHeadCell</th>
-    );
-  });
 });
