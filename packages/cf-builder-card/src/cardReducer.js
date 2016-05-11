aef31fe8bf9a1e992ee25ccb81a3cf7d45b166a1
James Kyle <me@thejameskyle.com>
2016-05-11 13:33:39 -0700

    Cleanup card builder

--- packages/cf-builder-card/src/cardReducer.js
+++ packages/cf-builder-card/src/cardReducer.js
@@ -1,18 +1,20 @@
 const ActionTypes = require('./CardActionTypes');
-const cf = require('cf-pony');
 
 const initialState = {};
 
-function cardReducer(state = {}, action) {
+function cardReducer(state = initialState, action) {
   switch (action.type) {
     case ActionTypes.CF_BUILDER_CARD_DRAWER_TOGGLE:
       const {cardName, drawerId} = action;
       const prevActiveDrawer = state[cardName] && state[cardName].activeDrawer;
-      return cf.objectAssign({}, state, {
-        [cardName]: cf.objectAssign({}, state[cardName], {
+
+      return {
+        ...state,
+        [cardName]: {
+          ...state[cardName],
           activeDrawer: drawerId === prevActiveDrawer ? null : drawerId
-        })
-      });
+        }
+      };
 
     default:
       return state;
