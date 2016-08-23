bb2e5bd11afbd6eca09a14c55a8ae04fbe53feaf
Furkan Yilmaz <mufuyil@gmail.com>
2016-08-23 17:45:57 +0100

    Added footer message for cf-component-card/CardContent

--- packages/cf-component-card/test/CardContent.js
+++ packages/cf-component-card/test/CardContent.js
@@ -3,13 +3,27 @@ const assertEqualJSX = require('assert-equal-jsx');
 const CardContent = require('../src/CardContent');
 
 describe('CardContent', function() {
-  it('should render', function() {
+  it('should render without footer message', function() {
     assertEqualJSX(
       <CardContent title="Title">CardContent</CardContent>,
       // should equal
       <div className="cf-card__content">
         <h3 className="cf-card__title">Title</h3>
         CardContent
+      </div>
+    );
+  });
+
+  it('should render with footer message', function() {
+    assertEqualJSX(
+      <CardContent title="Title" footerMessage="Footer Message">CardContent</CardContent>,
+      // should equal
+      <div className="cf-card__content">
+        <h3 className="cf-card__title">Title</h3>
+        CardContent
+        <div className="cf-card__footer_message">
+          Footer Message
+        </div>
       </div>
     );
   });
