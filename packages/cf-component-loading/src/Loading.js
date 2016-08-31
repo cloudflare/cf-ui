13b1afc6dc839872b8b197249d20f039e71cea3a
John Wineman <jwineman@cloudflare.com>
2016-08-31 12:19:05 -0700

    PI-764: Updating cf-component-loading to depend on cf-component-icon styles.

--- packages/cf-component-loading/src/Loading.js
+++ packages/cf-component-loading/src/Loading.js
@@ -3,8 +3,8 @@ const React = require('react');
 class Loading extends React.Component {
   render() {
     return (
-      <div className="cf-loading" role="status">
-        <span className="cf-loading__label">Loading</span>
+      <div className="cf-icon--loading" role="status">
+        <span className="cf-icon--loading__span">Loading</span>
       </div>
     );
   }
