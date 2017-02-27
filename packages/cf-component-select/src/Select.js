6ac88e6746c75a5800dda5c11a4b493c61881bfa
Kristján Oddsson <koddsson@gmail.com>
2017-02-27 19:55:16 +0000

    Send all props down to react-select. (#85)
    I could also c/p the props from the react-select documentation if we'd
    prefer that :D

--- packages/cf-component-select/src/Select.js
+++ packages/cf-component-select/src/Select.js
@@ -4,55 +4,20 @@ const ReactSelect = require('react-select');
 
 class Select extends React.Component {
   render() {
-    const SelectClass = this.props.async ? ReactSelect.Async : ReactSelect;
+    const { async, label, ...props } = this.props;
+    const SelectClass = async ? ReactSelect.Async : ReactSelect;
     return (
       <div className="cf-select">
-        {this.props.label && <label>{this.props.label}</label>}
+        {label && <label>{label}</label>}
         <SelectClass
-          // Simplify if this isnt a searchable select
-          searchable={this.props.searchable}
+          {...props}
           clearable={this.props.searchable}
           backspaceRemoves={this.props.searchable}
-          onChange={this.props.onChange}
-          onBlur={this.props.onBlur}
-          onFocus={this.props.onFocus}
-          loadOptions={this.props.loadOptions}
-          multi={this.props.multi}
-          value={this.props.value}
-          options={this.props.options}
-          placeholder={this.props.placeholder}
-          disabled={this.props.disabled}
-          cache={this.props.cache}
         />
       </div>
     );
   }
 }
-
-Select.propTypes = {
-  label: PropTypes.string,
-
-  onChange: PropTypes.func.isRequired,
-  onBlur: PropTypes.func,
-  onFocus: PropTypes.func,
-  loadOptions: PropTypes.func,
-
-  multi: PropTypes.bool,
-  searchable: PropTypes.bool,
-  async: PropTypes.bool,
-  disabled: PropTypes.bool,
-  cache: PropTypes.bool,
-
-  value: PropTypes.any,
-  options: PropTypes.arrayOf(
-    PropTypes.shape({
-      label: PropTypes.string.isRequired,
-      value: PropTypes.any.isRequired,
-      disabled: PropTypes.bool
-    })
-  ),
-  placeholder: PropTypes.string
-};
 
 Select.defaultProps = {
   multi: false,
