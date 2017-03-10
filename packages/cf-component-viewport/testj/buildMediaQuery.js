fab93e4673a71258019d8b6af5e6c51c2394f6c0
Yevgen Safronobv <yevgen@cloudflare.com>
2017-03-10 17:03:00 +0000

    migrate cf-component-viewport tests to Jest

--- packages/cf-component-viewport/testj/buildMediaQuery.js
+++ packages/cf-component-viewport/testj/buildMediaQuery.js
@@ -1,37 +1,37 @@
-const { equal: assertEqual } = require('assert');
-const buildMediaQuery = require('../src/buildMediaQuery');
+import { equal as assertEqual } from 'assert';
+import buildMediaQuery from '../src/buildMediaQuery';
 
-describe('buildMediaQuery', function() {
-  it('should build minWidth query', function() {
+describe('buildMediaQuery', () => {
+  test('should build minWidth query', () => {
     assertEqual(buildMediaQuery({ minWidth: '10em' }), '(min-width: 10em)');
   });
 
-  it('should build maxWidth query', function() {
+  test('should build maxWidth query', () => {
     assertEqual(buildMediaQuery({ maxWidth: '20em' }), '(max-width: 20em)');
   });
 
-  it('should build minWidth/maxWidth query', function() {
+  test('should build minWidth/maxWidth query', () => {
     assertEqual(
       buildMediaQuery({ minWidth: '10em', maxWidth: '20em' }),
       '(min-width: 10em) and (max-width: 20em)'
     );
   });
 
-  it('should build minWidth/not query', function() {
+  test('should build minWidth/not query', () => {
     assertEqual(
       buildMediaQuery({ minWidth: '10em' }, true),
       'not all and (min-width: 10em)'
     );
   });
 
-  it('should build maxWidth/not query', function() {
+  test('should build maxWidth/not query', () => {
     assertEqual(
       buildMediaQuery({ maxWidth: '20em' }, true),
       'not all and (max-width: 20em)'
     );
   });
 
-  it('should build minWidth/maxWidth/not query', function() {
+  test('should build minWidth/maxWidth/not query', () => {
     assertEqual(
       buildMediaQuery({ minWidth: '10em', maxWidth: '20em' }, true),
       'not all and (min-width: 10em) and (max-width: 20em)'
