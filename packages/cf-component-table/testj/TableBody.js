38a289e46b6692125bee3ce6642f129af853a059
Yevgen Safronobv <yevgen@cloudflare.com>
2017-03-10 16:28:19 +0000

    migrate cf-component-table tests to Jest

---
+++ packages/cf-component-table/testj/TableBody.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { TableBody } from 'cf-component-table';
+
+describe('TableBody', () => {
+  test('should render', () => {
+    const component = renderer.create(<TableBody>TableBody</TableBody>);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test('should render extra class names', () => {
+    const component = renderer.create(
+      <TableBody className="extra">TableBody</TableBody>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
---
+++ packages/cf-component-table/testj/TableRow.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { TableRow } from 'cf-component-table';
+
+describe('TableRow', () => {
+  test('should render', () => {
+    const component = renderer.create(<TableRow>TableRow</TableRow>);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test('should render extra class name', () => {
+    const component = renderer.create(
+      <TableRow className="extra">TableRow</TableRow>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test('should render with type', () => {
+    const component = renderer.create(
+      <TableRow type="error">TableRow</TableRow>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test('should render with accent', () => {
+    const component = renderer.create(
+      <TableRow accent="orange">TableRow</TableRow>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
