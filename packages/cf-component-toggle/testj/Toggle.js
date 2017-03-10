0717514aa6fc482a0c59f642a5a3a238104ee11b
Yevgen Safronobv <yevgen@cloudflare.com>
2017-03-10 16:54:14 +0000

    migrate cf-component-toggle tests to Jest

---
+++ packages/cf-component-toggle/testj/Toggle.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Toggle from 'cf-component-toggle';
+
+describe('Toggle', function() {
+  test('should render', () => {
+    const component = renderer.create(
+      <Toggle label="Test Toggle" name="test" />
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test('should render checked', () => {
+    const component = renderer.create(
+      <Toggle label="Test Toggle" name="test" value={true} />
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test('should render disabled', () => {
+    const component = renderer.create(
+      <Toggle disabled label="Test Toggle" name="test" value={true} />
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
