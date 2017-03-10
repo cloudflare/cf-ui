34cedd8a69bbef35a793f5f2dff2acd4fa36af17
Yevgen Safronobv <yevgen@cloudflare.com>
2017-03-10 15:40:54 +0000

    migrate cf-component-progress test to Jest

---
+++ packages/cf-component-progress/testj/Progress.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Progress from 'cf-component-progress';
+import Link from 'cf-component-link';
+
+describe('Progress', function() {
+  test('should render', function() {
+    const component = renderer.create(
+      <Progress
+        active="foo"
+        onChange={() => {}}
+        steps={[
+          { id: 'foo', label: 'Foo', disabled: false },
+          { id: 'bar', label: 'Bar', disabled: false },
+          { id: 'baz', label: 'Baz', disabled: true }
+        ]}
+      />
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
