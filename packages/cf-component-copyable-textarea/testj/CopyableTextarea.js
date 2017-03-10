8c9eab4fb1fb87652bf090ecb92953c15b73150a
tajo <vojtech@miksu.cz>
2017-03-09 17:21:31 -0800

    add tests for cf-component-copyable-textarea

---
+++ packages/cf-component-copyable-textarea/testj/CopyableTextarea.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import jsdom from 'jsdom';
+import { mount } from 'enzyme';
+import CopyableTextarea from 'cf-component-copyable-textarea';
+
+beforeAll(() => {
+  const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+  global.document = doc;
+  global.window = doc.defaultView;
+  global.document.execCommand = () => true;
+});
+
+test('should render', () => {
+  const component = renderer.create(
+    <CopyableTextarea
+      name="example"
+      value="content"
+      clickToCopyText="click here"
+      copiedTextToClipboardText="copied"
+      pressCommandOrCtrlCToCopyText="press here"
+    />
+  );
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('should call onCopy on focus', () => {
+  let clicked = false;
+  const copyableTextarea = mount(
+    <CopyableTextarea
+      name="example"
+      value="content"
+      onCopy={() => clicked = true}
+    />
+  );
+  copyableTextarea.find('textarea').simulate('focus');
+  expect(clicked).toBeTruthy();
+});
+
+test('should render copied help text after copying', () => {
+  const copyableTextarea = mount(
+    <CopyableTextarea
+      name="example"
+      value="content"
+      copiedTextToClipboardText="copied"
+    />
+  );
+  copyableTextarea.find('textarea').simulate('focus');
+  expect(copyableTextarea.find('.cf-copyable-textarea__help-text').text()).toBe(
+    'copied'
+  );
+});
+
+test('should render press help text if copying was not successful', () => {
+  global.document.execCommand = () => {
+    throw new Error('error');
+  };
+  const copyableTextarea = mount(
+    <CopyableTextarea
+      name="example"
+      value="content"
+      pressCommandOrCtrlCToCopyText="press here"
+    />
+  );
+  copyableTextarea.find('textarea').simulate('focus');
+  expect(copyableTextarea.find('.cf-copyable-textarea__help-text').text()).toBe(
+    'press here'
+  );
+});
