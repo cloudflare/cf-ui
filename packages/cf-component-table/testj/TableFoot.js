82fc8331f1e6dfbd8e11855e2f6b8c968103d8ea
Yevgen Safronobv <yevgen@cloudflare.com>
2017-03-10 16:29:25 +0000

    update to common style

--- packages/cf-component-table/testj/TableFoot.js
+++ packages/cf-component-table/testj/TableFoot.js
@@ -2,13 +2,13 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { TableFoot } from 'cf-component-table';
 
-describe('TableFoot', function() {
-  test('should render', function() {
+describe('TableFoot', () => {
+  test('should render', () => {
     const component = renderer.create(<TableFoot>TableFoot</TableFoot>);
     expect(component.toJSON()).toMatchSnapshot();
   });
 
-  test('should render extra class name', function() {
+  test('should render extra class name', () => {
     const component = renderer.create(
       <TableFoot className="extra">TableFoot</TableFoot>
     );
