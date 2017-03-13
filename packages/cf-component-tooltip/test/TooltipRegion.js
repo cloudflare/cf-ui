a74edb14c478b69d313430ecd0a01bbc7eb1296b
tajo <vojtech@miksu.cz>
2017-03-13 16:39:03 -0700

    rename testj to test since all old tests are gone, yay

--- packages/cf-component-tooltip/test/TooltipRegion.js
+++
@@ -1,5 +0,0 @@
-const React = require('react');
-const assertEqualJSX = require('assert-equal-jsx');
-const TooltipRegion = require('../src/TooltipRegion');
-
-console.warn('No TooltipRegion tests.');
