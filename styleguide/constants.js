4ba793f9e6b83c470e8b8aae32ce319435e4fb54
tajo <vojtech@miksu.cz>
2017-03-15 15:14:53 -0700

    remove styleguide

--- styleguide/constants.js
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-
-const ABSOLUTE_BASE = path.normalize(path.join(__dirname, '..'));
-
-export const BUILD_DIR = path.join(ABSOLUTE_BASE, 'docs');
-export const SRC_DIR = path.join(ABSOLUTE_BASE, 'styleguide');
-export const HOT_RELOAD_PORT = process.env.HOT_RELOAD_PORT || 8080;
