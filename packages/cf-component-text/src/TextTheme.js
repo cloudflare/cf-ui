5b063ea0b7896326d8b32a1c4a81c5549a6fa11a
Yevgen Safronov <sejoker@gmail.com>
2017-03-17 15:30:44 +0000

    move color declarations into cf-style-const

--- packages/cf-component-text/src/TextTheme.js
+++ packages/cf-component-text/src/TextTheme.js
@@ -1,19 +1,24 @@
-export default () => ({
-  fontSizeNormal: '1em',
-  fontWeightNormal: 400,
-  fontSizeNormal: '.8em',
-  lineHeightSmall: 1.3,
-  textAlignStart: 'left',
-  textAlignCenter: 'center',
-  textAlignJustify: 'justify',
-  textAlignEnd: 'right',
-  textTransformCapitalize: 'capitalize',
-  'textTransformCapitalize:first-word': 'capitalize',
-  textTransformLowercase: 'lowercase',
-  textTransformUppercase: 'uppercase',
-  colorInfo: '#00a9eb',
-  colorSuccess: '#68970f',
-  colorWarning: '#fca520',
-  colorError: '#ff3100',
-  colorMuted: '#gray'
-});
+import { checkBaseTheme } from 'cf-style-const';
+
+export default baseTheme => {
+  checkBaseTheme(baseTheme, 'TextTheme');
+  return {
+    fontSizeNormal: '1em',
+    fontWeightNormal: 400,
+    fontSizeNormal: '.8em',
+    lineHeightSmall: 1.3,
+    textAlignStart: 'left',
+    textAlignCenter: 'center',
+    textAlignJustify: 'justify',
+    textAlignEnd: 'right',
+    textTransformCapitalize: 'capitalize',
+    'textTransformCapitalize:first-word': 'capitalize',
+    textTransformLowercase: 'lowercase',
+    textTransformUppercase: 'uppercase',
+    colorInfo: '#00a9eb',
+    colorSuccess: baseTheme.colorSuccess,
+    colorWarning: baseTheme.colorWarning,
+    colorError: baseTheme.colorError,
+    colorMuted: baseTheme.colorMuted
+  };
+};
