fc7e566489a0ede71a9d79d99a58925e1c1c3780
Yevgen Safronov <sejoker@gmail.com>
2017-03-22 16:58:01 +0000

    using plain object for styling in heading caption

--- packages/cf-component-heading/src/HeadingCaption.js
+++ packages/cf-component-heading/src/HeadingCaption.js
@@ -1,16 +1,13 @@
 import React, { PropTypes } from 'react';
 import { createComponent } from 'cf-style-container';
 
-const styles = props => {
-  const theme = props.theme;
-  return {
-    color: theme.color,
-    fontSize: theme.fontFize,
-    fontColor: theme.fontColor,
-    fontWeight: theme.fontWeight,
-    marginLeft: theme.marginLeft
-  };
-};
+const styles = ({ theme }) => ({
+  color: theme.color,
+  fontSize: theme.fontFize,
+  fontColor: theme.fontColor,
+  fontWeight: theme.fontWeight,
+  marginLeft: theme.marginLeft
+});
 
 const HeadingCaption = ({ className, children }) => (
   <small className={className}>
