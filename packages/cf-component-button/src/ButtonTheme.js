9f5a867b0b3bf286f95f12d85dd43f3eee18b767
tajo <vojtech@miksu.cz>
2017-03-23 14:23:04 -0700

    move private styles (consts and button themes) to cf-ui

--- packages/cf-component-button/src/ButtonTheme.js
+++ packages/cf-component-button/src/ButtonTheme.js
@@ -3,74 +3,173 @@ import { checkBaseTheme } from 'cf-style-const';
 
 export default baseTheme => {
   checkBaseTheme(baseTheme, 'ButtonTheme');
+  const defaultBorder = color(
+    `${baseTheme.colorGrayLight} lightness(${baseTheme.colorOffsetDark})`
+  );
+  const primaryBorder = color(
+    `${baseTheme.colorBlue} lightness(${baseTheme.colorOffsetDark})`
+  );
+  const successBorder = color(
+    `${baseTheme.colorGreen} lightness(${baseTheme.colorOffsetDark})`
+  );
+  const warningBorder = color(
+    `${baseTheme.colorOrange} lightness(${baseTheme.colorOffsetDark})`
+  );
+  const dangerBorder = color(
+    `${baseTheme.colorRed}  lightness(${baseTheme.colorOffsetDark})`
+  );
+  const defaultHoverBackground = color(
+    `${baseTheme.colorGrayLight} lightness(${baseTheme.colorOffsetDark})`
+  );
+  const defaultActiveBackground = color(
+    `${defaultHoverBackground} lightness(${baseTheme.colorOffsetDark})`
+  );
+  const primaryActiveBackground = color(
+    `${baseTheme.colorBlueDark} lightness(${baseTheme.colorOffsetDark})`
+  );
+  const successActiveBackground = color(
+    `${baseTheme.colorGreenDark} lightness(${baseTheme.colorOffsetDark})`
+  );
+  const warningActiveBackground = color(
+    `${baseTheme.colorOrangeDark} lightness(${baseTheme.colorOffsetDark})`
+  );
+  const dangerActiveBackground = color(
+    `${baseTheme.colorRedDark} lightness(${baseTheme.colorOffsetDark})`
+  );
+
   return {
     fontSize: `${(parseFloat(baseTheme.fontSize) - 1) / parseFloat(baseTheme.fontSize)}rem`,
-    paddingTop: '0.7em',
+    fontWeight: baseTheme.weightNormal,
+    paddingTop: '0.6em',
     paddingRight: '1em',
-    paddingBottom: '0.4em',
+    paddingBottom: '0.53em',
     paddingLeft: '1em',
     spacing: '0.4rem',
+    borderSize: '1px',
     display: 'inline-block',
     position: 'relative',
     marginTop: 0,
     marginBottom: 0,
     marginLeft: 0,
     marginRight: 0,
-    border: 'none',
-    borderBottom: '3px solid rgba(0, 0, 0, .2)',
     background: 'transparent',
     lineHeight: 1.2,
     textAlign: 'center',
+    webkitTextStroke: 0, // This makes white text in buttons hard to read, so removing it
+    webkitFontSmoothing: 'subpixel-antialiased',
     userSelect: 'none',
     cursor: 'pointer',
     transition: 'all 200ms ease',
+    border: 'none',
     borderLeft: 0,
+    borderBottom: '1px solid',
     borderTop: 0,
     borderRight: 0,
-    boxShadow: '0 1px 2px rgba(0, 0, 0, .1)',
-    defaultBorder: color(
-      `${baseTheme.defaultBackground} lightness(${baseTheme.colorOffsetDark})`
-    ),
-    primaryBorder: color(
-      `${baseTheme.primaryBackground} lightness(${baseTheme.colorOffsetDark})`
-    ),
-    successBorder: color(
-      `${baseTheme.successBackground} lightness(${baseTheme.colorOffsetDark})`
-    ),
-    warningBorder: color(
-      `${baseTheme.warningBackground} lightness(${baseTheme.colorOffsetDark})`
-    ),
-    dangerBorder: color(
-      `${baseTheme.dangerBackground}  lightness(${baseTheme.colorOffsetDark})`
-    ),
+
+    // Disabled
     disabledBorder: color(
       `${baseTheme.disabledBackground} lightness(${baseTheme.colorOffsetDark})`
     ),
-    defaultColor: baseTheme.colorWhite,
+    disabledColor: color(`${baseTheme.disabledBackground} lightness(30%)`),
+
+    // Normal
+    defaultBackground: baseTheme.colorGrayLight,
+    primaryBackground: baseTheme.colorBlue,
+    successBackground: baseTheme.colorGreen,
+    warningBackground: baseTheme.colorOrange,
+    dangerBackground: baseTheme.colorRed,
+
+    defaultBorder,
+    primaryBorder,
+    successBorder,
+    warningBorder,
+    dangerBorder,
+
+    defaultColor: baseTheme.colorGrayDark,
     primaryColor: baseTheme.colorWhite,
     successColor: baseTheme.colorWhite,
     warningColor: baseTheme.colorWhite,
     dangerColor: baseTheme.colorWhite,
-    disabledColor: color(`${baseTheme.disabledBackground} lightness(30%)`),
-    defaultHoverBackground: color(
-      `${baseTheme.defaultBackground} lightness(${baseTheme.colorOffsetDark})`
+
+    // Hover
+    defaultHoverBackground,
+    primaryHoverBackground: baseTheme.colorBlueDark,
+    successHoverBackground: baseTheme.colorGreenDark,
+    warningHoverBackground: baseTheme.colorOrangeDark,
+    dangerHoverBackground: baseTheme.colorRedDark,
+
+    defaultHoverBorder: color(
+      `${defaultBorder} lightness(${baseTheme.colorOffsetDark})`
     ),
-    primaryHoverBackground: color(
-      `${baseTheme.primaryBackground} lightness(${baseTheme.colorOffsetDark})`
+    primaryHoverBorder: color(
+      `${primaryBorder} lightness(${baseTheme.colorOffsetDark})`
     ),
-    successHoverBackground: color(
-      `${baseTheme.successBackground} lightness(${baseTheme.colorOffsetDark})`
+    successHoverBorder: color(
+      `${baseTheme.colorGreenDark} lightness(${baseTheme.colorOffsetDark})`
     ),
-    warningHoverBackground: color(
-      `${baseTheme.warningBackground} lightness(${baseTheme.colorOffsetDark})`
+    warningHoverBorder: color(
+      `${baseTheme.colorOrangeDark} lightness(${baseTheme.colorOffsetDark})`
     ),
-    dangerHoverBackground: color(
-      `${baseTheme.dangerBackground} lightness(${baseTheme.colorOffsetDark})`
+    dangerHoverBorder: color(
+      `${baseTheme.colorRedDark}  lightness(${baseTheme.colorOffsetDark})`
     ),
-    defaultHoverColor: baseTheme.colorWhite,
+
+    defaultHoverColor: baseTheme.colorGrayDark,
     primaryHoverColor: baseTheme.colorWhite,
     successHoverColor: baseTheme.colorWhite,
     warningHoverColor: baseTheme.colorWhite,
-    dangerHoverColor: baseTheme.colorWhite
+    dangerHoverColor: baseTheme.colorWhite,
+
+    // Active
+    defaultActiveBackground,
+    primaryActiveBackground,
+    successActiveBackground,
+    warningActiveBackground,
+    dangerActiveBackground,
+
+    defaultActiveBorder: color(
+      `${defaultActiveBackground} lightness(${baseTheme.colorOffsetDark})`
+    ),
+    primaryActiveBorder: color(
+      `${primaryActiveBackground} lightness(${baseTheme.colorOffsetDark})`
+    ),
+    successActiveBorder: color(
+      `${successActiveBackground} lightness(${baseTheme.colorOffsetDark})`
+    ),
+    warningActiveBorder: color(
+      `${warningActiveBackground} lightness(${baseTheme.colorOffsetDark})`
+    ),
+    dangerActiveBorder: color(
+      `${dangerActiveBackground}  lightness(${baseTheme.colorOffsetDark})`
+    ),
+
+    defaultActiveColor: baseTheme.colorGrayDark,
+    primaryActiveColor: baseTheme.colorWhite,
+    successActiveColor: baseTheme.colorWhite,
+    warningActiveColor: baseTheme.colorWhite,
+    dangerActiveColor: baseTheme.colorWhite,
+
+    // Focus
+    defaultFocusColor: baseTheme.colorGrayDark,
+    primaryFocusColor: baseTheme.colorWhite,
+    successFocusColor: baseTheme.colorWhite,
+    warningFocusColor: baseTheme.colorWhite,
+    dangerFocusColor: baseTheme.colorWhite,
+
+    defaultFocusOutlineColor: color(
+      `${defaultActiveBackground} lightness(${baseTheme.colorOffsetDark})`
+    ),
+    primaryFocusOutlineColor: color(
+      `${primaryActiveBackground} lightness(${baseTheme.colorOffsetDark})`
+    ),
+    successFocusOutlineColor: color(
+      `${successActiveBackground} lightness(${baseTheme.colorOffsetDark})`
+    ),
+    warningFocusOutlineColor: color(
+      `${warningActiveBackground} lightness(${baseTheme.colorOffsetDark})`
+    ),
+    dangerFocusOutlineColor: color(
+      `${dangerActiveBackground} lightness(${baseTheme.colorOffsetDark})`
+    )
   };
 };
