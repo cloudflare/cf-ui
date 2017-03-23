2c9ac2b3dd423e84e2ca023be52f4d6c45ea7508
tajo <vojtech@miksu.cz>
2017-03-23 14:26:09 -0700

    move private styles for cf-component-heading to cf-ui

--- packages/cf-component-heading/src/HeadingTheme.js
+++ packages/cf-component-heading/src/HeadingTheme.js
@@ -1,7 +1,20 @@
 export default () => ({
-  fontSize1: '1.8em',
-  fontSize2: '1.6em',
-  fontSize3: '1.4em',
-  fontSize4: '1.2em',
-  marginTop2: '10px'
+  fontSize1: '2rem',
+  lineHeight1: 1.2,
+  fontWeight1: 700,
+  fontSize2: '1.6rem',
+  lineHeight2: 1.3,
+  fontWeight3: 700,
+  fontSize3: '1.46667rem',
+  lineHeight3: 1.3,
+  fontWeight3: 700,
+  fontSize4: '1.2rem',
+  lineHeight4: 1.3,
+  fontWeight4: 700,
+  fontSize5: '1rem',
+  lineHeight5: 1.5,
+  fontWeight5: 700,
+  fontSize6: '.93333rem',
+  lineHeight6: 1.6,
+  fontWeight6: 700
 });
