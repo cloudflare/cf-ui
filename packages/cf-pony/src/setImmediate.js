1f58f05b4a6c5d82c23acba7e0a30c684e1d34c6
Jimmy Yuen Ho Wong <wyuenho@users.noreply.github.com>
2017-03-30 00:20:20 +0100

    Issue #50 remove useless packages (#130)
    * Issue #50 remove useless packages
    
    * Issue-50 remove cf-builder-table dependency on cf-pony

--- packages/cf-pony/src/setImmediate.js
+++
@@ -1,6 +0,0 @@
-// The setimmediate module is a polyfill that sets the global setImmediate...
-// Ponies are better.
-var prevSetImmediate = global.setImmediate;
-import setImmediate from 'setimmediate';
-export default setImmediate;
-global.setImmediate = prevSetImmediate;
