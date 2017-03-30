a59fcbc7536c6bc2b3163c61fb979ec164de5ff1
Yevgen Safronov <sejoker@gmail.com>
2017-03-30 12:53:17 +0100

    split PaginationLink from PaginationItem

--- packages/cf-component-pagination/src/PaginationItem.js
+++ packages/cf-component-pagination/src/PaginationItem.js
@@ -1,65 +1,47 @@
-import React, { PropTypes } from 'react';
-import Icon from 'cf-component-icon';
+import React, { PropTypes } from "react";
+import Icon from "cf-component-icon";
+import PaginationLink from "./PaginationLink";
 
-class PaginationItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
+const PaginationItem = props => {
+  let className = "cf-pagination__item cf-pagination__item--" + props.type;
+
+  if (props.active) className += " cf-pagination__item--active";
+  if (props.disabled) className += " cf-pagination__item--disabled";
+
+  const isEllipsis = props.type === "ellipsis";
+  const isLoading = props.type === "loading";
+
+  const role = isEllipsis ? "presentation" : null;
+
+  let children;
+
+  if (isEllipsis) {
+    children = <span>…</span>;
+  } else if (isLoading) {
+    children = <Icon type="loading" label={false} />;
+  } else {
+    children = props.children;
   }
-
-  handleClick(e) {
-    e.preventDefault();
-    this.props.onClick();
-  }
-
-  render() {
-    let className = 'cf-pagination__item cf-pagination__item--' +
-      this.props.type;
-
-    if (this.props.active) className += ' cf-pagination__item--active';
-    if (this.props.disabled) className += ' cf-pagination__item--disabled';
-
-    const isEllipsis = this.props.type === 'ellipsis';
-    const isLoading = this.props.type === 'loading';
-
-    const role = isEllipsis ? 'presentation' : null;
-
-    let children;
-
-    if (isEllipsis) {
-      children = <span>…</span>;
-    } else if (isLoading) {
-      children = <Icon type="loading" label={false} />;
-    } else {
-      children = this.props.children;
-    }
-
-    return (
-      <li className={className} role={role}>
-        {this.props.active || this.props.disabled || isEllipsis
-          ? <span className="cf-pagination__link" aria-label={this.props.label}>
-              {children}
-            </span>
-          : <a
-              className="cf-pagination__link"
-              href="#"
-              onClick={this.handleClick}
-              aria-label={this.props.label}
-            >
-              {children}
-            </a>}
-      </li>
-    );
-  }
-}
+  const clickable = !(props.active || props.disabled || isEllipsis);
+  return (
+    <li className={className} role={role}>
+      <PaginationLink
+        onClick={props.onClick}
+        clickable={clickable}
+        children={children}
+        label={props.label}
+      />
+    </li>
+  );
+};
 
 PaginationItem.propTypes = {
   type: PropTypes.oneOf([
-    'number',
-    'next',
-    'prev',
-    'ellipsis',
-    'loading'
+    "number",
+    "next",
+    "prev",
+    "ellipsis",
+    "loading"
   ]).isRequired,
   label: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
