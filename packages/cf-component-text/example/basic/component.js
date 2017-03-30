7c424c57034dc88abbf8eeb01e9ac5f71a34f40f
tajo <vojtech@miksu.cz>
2017-03-29 17:08:56 -0700

    export styled cf-component-text by default

--- packages/cf-component-text/example/basic/component.js
+++ packages/cf-component-text/example/basic/component.js
@@ -1,33 +1,30 @@
 import React from 'react';
-import { Text, TextTheme } from 'cf-component-text';
-import { applyTheme } from 'cf-style-container';
-
-const StyledText = applyTheme(Text, TextTheme);
+import { Text } from 'cf-component-text';
 
 const TextComponent = () => (
   <div>
     <p>Specify a <code>size</code></p>
-    <StyledText size="normal">Hello World</StyledText>
-    <StyledText size="small">Hello World</StyledText>
+    <Text size="normal">Hello World</Text>
+    <Text size="small">Hello World</Text>
 
     <p>and/or an <code>align</code></p>
-    <StyledText align="start">Hello World</StyledText>
-    <StyledText align="center">Hello World</StyledText>
-    <StyledText align="justify">Hello World</StyledText>
-    <StyledText align="end">Hello World</StyledText>
+    <Text align="start">Hello World</Text>
+    <Text align="center">Hello World</Text>
+    <Text align="justify">Hello World</Text>
+    <Text align="end">Hello World</Text>
 
     <p>and/or a <code>type</code></p>
-    <StyledText type="info">Hello World</StyledText>
-    <StyledText type="success">Hello World</StyledText>
-    <StyledText type="warning">Hello World</StyledText>
-    <StyledText type="error">Hello World</StyledText>
-    <StyledText type="muted">Hello World</StyledText>
+    <Text type="info">Hello World</Text>
+    <Text type="success">Hello World</Text>
+    <Text type="warning">Hello World</Text>
+    <Text type="error">Hello World</Text>
+    <Text type="muted">Hello World</Text>
 
     <p>and/or a <code>case</code></p>
-    <StyledText case="capitalize">hello world</StyledText>
-    <StyledText case="titlecase">hello world</StyledText>
-    <StyledText case="lowercase">Hello World</StyledText>
-    <StyledText case="uppercase">Hello World</StyledText>
+    <Text case="capitalize">hello world</Text>
+    <Text case="titlecase">hello world</Text>
+    <Text case="lowercase">Hello World</Text>
+    <Text case="uppercase">Hello World</Text>
   </div>
 );
 
