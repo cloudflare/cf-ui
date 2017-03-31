c58d9a22e81c79ffc11d147bccc03d24d5943e84
Kristján Oddsson <koddsson@gmail.com>
2017-03-31 09:29:17 +0100

    card: Keep a local state of drawers if no props (#125)
    * card: Keep a local state of drawers if no props
    
    Whenever you're using the `CardDrawers` sub-component of `Card` you need
    to add some keys to your state and handle all the events yourself. This
    is fine if you want to control the flow very tightly but the most common
    case is just to switch to the drawers selected.
    
    This patch changes the `CardDrawers` sub-component so it still takes in
    the props and those will control the component but falls back to the
    local state if the props `active` and `onClick` are not present.
    
    * card: Explicitly check for active prop in drawsers
    
    Since the state property can have the very reasonable value `null` which
    signifies no drawer should be shown we need to make a explicit
    `hasOwnProperty` call to make sure that the `active` prop has been set
    and isn't just falsy because it wasn't set.

--- packages/cf-component-card/test/CardDrawers.js
+++ packages/cf-component-card/test/CardDrawers.js
@@ -6,11 +6,23 @@ import {
   CardToolbarLink
 } from '../../cf-component-card/src/index';
 
-test('should render', () => {
+test('should render when given all the props', () => {
   const component = renderer.create(
     <CardDrawers
       onClick={() => {}}
       active="one"
+      drawers={[
+        { id: 'one', name: 'One', content: 'One Content' },
+        { id: 'two', name: 'Two', content: 'Two Content' }
+      ]}
+    />
+  );
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('should render when not given props', () => {
+  const component = renderer.create(
+    <CardDrawers
       drawers={[
         { id: 'one', name: 'One', content: 'One Content' },
         { id: 'two', name: 'Two', content: 'Two Content' }
