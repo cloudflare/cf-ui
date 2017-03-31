5de1d8411fcde792f6794ff000b3f57896de8b6c
tajo <vojtech@miksu.cz>
2017-03-31 16:13:52 -0700

    fix Form tests, update README, example

--- packages/cf-component-form/test/FormFieldError.js
+++ packages/cf-component-form/test/FormFieldError.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { renderToStaticMarkup } from 'react-dom/server';
-import { FormFieldError } from '../../cf-component-form/src/index';
+import { FormFieldError } from '../src/index';
+import felaTestContext from '../../../felaTestContext';
 
 test('should render valid state', () => {
   expect(
-    renderToStaticMarkup(<FormFieldError field={{}} validations={{}} />)
+    renderToStaticMarkup(
+      felaTestContext(<FormFieldError field={{}} validations={{}} />)
+    )
   ).toBe('');
 });
 
 test('should render invalid state', () => {
   const component = renderer.create(
-    <FormFieldError field={{ invalid: true }} validations={{}} />
+    felaTestContext(
+      <FormFieldError field={{ invalid: true }} validations={{}} />
+    )
   );
   expect(component.toJSON()).toMatchSnapshot();
 });
 
 test('should render invalid state with validation errors', () => {
   const component = renderer.create(
-    <FormFieldError
-      field={{ invalid: true }}
-      validations={{ required: 'This is required' }}
-    />
+    felaTestContext(
+      <FormFieldError
+        field={{ invalid: true }}
+        validations={{ required: 'This is required' }}
+      />
+    )
   );
   expect(component.toJSON()).toMatchSnapshot();
 });
--- packages/cf-component-form/test/FormLabel.js
+++ packages/cf-component-form/test/FormLabel.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { FormLabel } from '../../cf-component-form/src/index';
+import { FormLabel } from '../src/index';
+import felaTestContext from '../../../felaTestContext';
 
 test('should render', () => {
-  const component = renderer.create(<FormLabel>FormLabel</FormLabel>);
+  const component = renderer.create(
+    felaTestContext(<FormLabel>FormLabel</FormLabel>)
+  );
   expect(component.toJSON()).toMatchSnapshot();
 });
 
 test('should render hidden', function() {
-  const component = renderer.create(<FormLabel hidden>FormLabel</FormLabel>);
+  const component = renderer.create(
+    felaTestContext(<FormLabel hidden>FormLabel</FormLabel>)
+  );
   expect(component.toJSON()).toMatchSnapshot();
 });
