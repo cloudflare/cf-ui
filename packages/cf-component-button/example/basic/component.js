181504249050903e7e6182b648be18c9ea086cad
tajo <vojtech@miksu.cz>
2017-04-03 22:57:28 -0700

    add spaced prop for ButtonGroup

--- packages/cf-component-button/example/basic/component.js
+++ packages/cf-component-button/example/basic/component.js
@@ -2,17 +2,28 @@ import React from 'react';
 import { Button, ButtonGroup } from 'cf-component-button';
 
 const ButtonComponent = () => (
-  <ButtonGroup>
-    <Button type="primary" onClick={() => console.log('Clicked One!')}>
-      Button One
-    </Button>
-    <Button type="default" onClick={() => console.log('Clicked Two!')}>
-      Button Two
-    </Button>
-    <Button type="success" onClick={() => console.log('Clicked Three!')}>
-      Button Three
-    </Button>
-  </ButtonGroup>
+  <div>
+    <ButtonGroup>
+      <Button type="primary" onClick={() => console.log('Clicked One!')}>
+        Button One
+      </Button>
+      <Button type="default" onClick={() => console.log('Clicked Two!')}>
+        Button Two
+      </Button>
+      <Button type="success" onClick={() => console.log('Clicked Three!')}>
+        Button Three
+      </Button>
+    </ButtonGroup>
+    <p>Button group with spacing</p>
+    <ButtonGroup spaced>
+      <Button type="success" onClick={() => console.log('Clicked Four!')}>
+        Button Four
+      </Button>
+      <Button type="danger" onClick={() => console.log('Clicked Five!')}>
+        Button Five
+      </Button>
+    </ButtonGroup>
+  </div>
 );
 
 export default ButtonComponent;
