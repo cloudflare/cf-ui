83f5c43c7dc21ddfe499d08e8064919e7c246439
tajo <vojtech@miksu.cz>
2017-04-04 18:12:02 -0700

    add test that StyleProvider renders specific styles (full Fela integration test)

--- packages/cf-style-provider/test/index.js
+++ packages/cf-style-provider/test/index.js
@@ -1,11 +1,55 @@
 import React, { PropTypes } from 'react';
 import { StyleProvider } from '../../cf-style-provider/src/index';
+import { createComponent } from '../../cf-style-container/src/index';
+import { Button } from '../../cf-component-button/src/index';
 import renderer from 'react-test-renderer';
+import { render } from 'react-dom';
 import { mount } from 'enzyme';
+import jsdom from 'jsdom';
 
 test('StyleProvider should render', () => {
   const component = renderer.create(<StyleProvider><div /></StyleProvider>);
   expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('StyleProvider should render styles', () => {
+  const doc = jsdom.jsdom(
+    `
+	  <!doctype html>
+	  <html>
+	  <head>
+	    <style id="stylesheet"></style>
+	    <style id="font-stylesheet"></style>
+	  </head>
+	  <asd></asd>
+	  <body>
+	    <div id="react-app"></div>
+	  </body>
+	  </html>`
+  );
+
+  global.document = doc;
+  global.window = doc.defaultView;
+
+  const fontNode = doc.getElementById('font-stylesheet');
+  const cssNode = doc.getElementById('stylesheet');
+  const htmlNode = doc.getElementById('react-app');
+
+  const Foo = createComponent(() => ({
+    margin: '10px',
+    color: 'red'
+  }));
+
+  render(
+    <StyleProvider fontNode={fontNode} cssNode={cssNode}>
+      <Foo />
+    </StyleProvider>,
+    htmlNode
+  );
+
+  expect(doc.documentElement.outerHTML).toMatch(
+    /.cf-a{margin:10px}.cf-b{color:red}/
+  );
 });
 
 test('StyleProvider should pass custom props through', () => {
