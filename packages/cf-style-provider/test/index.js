42c06f2bf610e18b6628859569be9dc32111d583
tajo <vojtech@miksu.cz>
2017-04-04 17:14:46 -0700

    add test that StyleProvider passes custom props through

--- packages/cf-style-provider/test/index.js
+++ packages/cf-style-provider/test/index.js
@@ -1,8 +1,17 @@
 import React, { PropTypes } from 'react';
 import { StyleProvider } from '../../cf-style-provider/src/index';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 
 test('StyleProvider should render', () => {
   const component = renderer.create(<StyleProvider><div /></StyleProvider>);
   expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('StyleProvider should pass custom props through', () => {
+  const Foo = () => <div />;
+  const wrapper = mount(<StyleProvider number={10}><Foo /></StyleProvider>);
+  wrapper.setProps({ something: true });
+  expect(wrapper.prop('something')).toBeTruthy();
+  expect(wrapper.prop('number')).toBe(10);
 });
