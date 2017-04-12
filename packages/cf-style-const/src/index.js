6e534a6108d02e61a85cafc84a4931fbd5d74655
tajo <vojtech@miksu.cz>
2017-04-11 18:06:26 -0700

    move checkBaseTheme from cf-style-const to cf-style-container

--- packages/cf-style-const/src/index.js
+++ packages/cf-style-const/src/index.js
@@ -1,14 +1,3 @@
 import variables from './variables';
 
-const checkBaseTheme = (baseTheme, name) => {
-  if (!baseTheme) {
-    throw Error(
-      `
-      You can't use ${name} without providing global theme from
-      cf-style-consts. You should wrap your app by <ThemeProvider />!
-    `
-    );
-  }
-};
-
-export { variables, checkBaseTheme };
+export { variables };
