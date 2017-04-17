447555ae68c560e9c115da8853c309b9f77b7a81
John Wineman <jwineman@cloudflare.com>
2017-04-17 09:55:14 -0700

    Removing cf-component-heading dependency for cf-component-modal::ModalTitle.

--- packages/cf-component-modal/src/ModalTitle.js
+++ packages/cf-component-modal/src/ModalTitle.js
@@ -1,15 +1,12 @@
 import React, { PropTypes } from 'react';
-import { Heading } from 'cf-component-heading';
 import { applyTheme } from 'cf-style-container';
 
 class ModalTitle extends React.Component {
   render() {
     return (
-      <div className="cf-modal__title">
-        <Heading size={3}>
-          {this.props.children}
-        </Heading>
-      </div>
+      <h3 className="cf-modal__title">
+        {this.props.children}
+      </h3>
     );
   }
 }
