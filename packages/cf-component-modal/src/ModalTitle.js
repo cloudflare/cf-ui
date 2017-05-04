fe1642a39bf05b3c7397453ac1fe6fb716719618
John Wineman <jwineman@cloudflare.com>
2017-05-04 15:32:41 -0700

    Modal now uses H1 instead of cf-component-heading.

--- packages/cf-component-modal/src/ModalTitle.js
+++ packages/cf-component-modal/src/ModalTitle.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Heading } from 'cf-component-heading';
 import { applyTheme } from 'cf-style-container';
 
 class ModalTitle extends React.Component {
   render() {
     return (
       <div className="cf-modal__title">
-        <Heading size={3}>
+        <h1 className="cf-modal__title">
           {this.props.children}
-        </Heading>
+        </h1>
       </div>
     );
   }
