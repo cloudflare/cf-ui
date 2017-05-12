66412b365a508de10a08adb0f933cc118c3ed3ff
Kristján Oddsson <kristjan@cloudflare.com>
2017-05-12 14:22:25 +0100

    icon: set types directly in proptypes
    Instead of keeping the types and sizes available to the `Icon` component
    in a different file and injecting it into the prop types we now do it
    explicitly in the component proptypes.
    
    This means that the documentation that's generated will show the types
    available instead of just `TYPES` and `SIZES`.
    
    Closes: #192

--- packages/cf-component-icon/example/basic/component.js
+++ packages/cf-component-icon/example/basic/component.js
@@ -1,18 +1,63 @@
 import React from 'react';
-import Icon, { TYPES, SIZES } from 'cf-component-icon';
 
 const IconComponent = () => (
   <div>
     <p>Types:</p>
     <p>
-      {TYPES.map(type => <Icon label={type} type={type} key={type} />)}
+      <Icon label="bolt" type="bolt" key="bolt" />
+      <Icon label="calendar" type="calendar" key="calendar" />
+      <Icon label="caret-down" type="caret-down" key="caret-down" />
+      <Icon label="caret-left" type="caret-left" key="caret-left" />
+      <Icon label="caret-right" type="caret-right" key="caret-right" />
+      <Icon label="caret-up" type="caret-up" key="caret-up" />
+      <Icon label="chart" type="chart" key="chart" />
+      <Icon label="chevron-down" type="chevron-down" key="chevron-down" />
+      <Icon label="chevron-left" type="chevron-left" key="chevron-left" />
+      <Icon label="chevron-right" type="chevron-right" key="chevron-right" />
+      <Icon label="chevron-up" type="chevron-up" key="chevron-up" />
+      <Icon label="clipboard" type="clipboard" key="clipboard" />
+      <Icon label="close" type="close" key="close" />
+      <Icon label="drive" type="drive" key="drive" />
+      <Icon label="exclamation-sign" type="exclamation-sign" key="exclamation-sign" />
+      <Icon label="facebook" type="facebook" key="facebook" />
+      <Icon label="file" type="file" key="file" />
+      <Icon label="filter" type="filter" key="filter" />
+      <Icon label="flowchart" type="flowchart" key="flowchart" />
+      <Icon label="gear" type="gear" key="gear" />
+      <Icon label="googleplus" type="googleplus" key="googleplus" />
+      <Icon label="hamburger" type="hamburger" key="hamburger" />
+      <Icon label="happy" type="happy" key="happy" />
+      <Icon label="help" type="help" key="help" />
+      <Icon label="info-sign" type="info-sign" key="info-sign" />
+      <Icon label="linkedin" type="linkedin" key="linkedin" />
+      <Icon label="list" type="list" key="list" />
+      <Icon label="loading" type="loading" key="loading" />
+      <Icon label="lock" type="lock" key="lock" />
+      <Icon label="ok" type="ok" key="ok" />
+      <Icon label="ok-sign" type="ok-sign" key="ok-sign" />
+      <Icon label="pause" type="pause" key="pause" />
+      <Icon label="plus" type="plus" key="plus" />
+      <Icon label="remove" type="remove" key="remove" />
+      <Icon label="remove-sign" type="remove-sign" key="remove-sign" />
+      <Icon label="resize-horizontal" type="resize-horizontal" key="resize-horizontal" />
+      <Icon label="sad" type="sad" key="sad" />
+      <Icon label="search" type="search" key="search" />
+      <Icon label="shield" type="shield" key="shield" />
+      <Icon label="speech" type="speech" key="speech" />
+      <Icon label="time" type="time" key="time" />
+      <Icon label="twitter" type="twitter" key="twitter" />
+      <Icon label="upload" type="upload" key="upload" />
+      <Icon label="widen" type="widen" key="widen" />
+      <Icon label="wrench" type="wrench" key="wrench" />
     </p>
 
     <p>Sizes:</p>
     <p>
-      {SIZES.map(size => (
-        <Icon label={size} type="gear" size={size} key={size} />
-      ))}
+      <Icon label="2x" type="gear" key="2x" />
+      <Icon label="3x" type="gear" key="3x" />
+      <Icon label="4x" type="gear" key="4x" />
+      <Icon label="large" type="gear" key="large" />
+      <Icon label="xlarge" type="gear" key="xlarge" />
     </p>
 
     <p>Border:</p>
