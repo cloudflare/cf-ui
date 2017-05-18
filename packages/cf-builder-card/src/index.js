080b3f315eb630227aad7c1dc6f8a3acbb8d6fe7
Kristján Oddsson <kristjan@cloudflare.com>
2017-05-18 17:22:30 +0100

    remove cf-builder-* components
    closes: #105

--- packages/cf-builder-card/src/index.js
+++
@@ -1,6 +0,0 @@
-import cardActions from './cardActions';
-import CardActionTypes from './CardActionTypes';
-import CardBuilder from './CardBuilder';
-import cardReducer from './cardReducer';
-
-export { cardActions, CardActionTypes, CardBuilder, cardReducer };
