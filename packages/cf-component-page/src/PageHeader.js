607b293e644ad4c4679cd915133ac6282977236c
Kristján Oddsson <koddsson@gmail.com>
2017-06-02 14:27:45 +0100

    cf-component-page subtitle (#236)
    * Updated PageHeader to render 'subtitle' as an h2
    
    * Updated PageHeader jest snapshot

--- packages/cf-component-page/src/PageHeader.js
+++ packages/cf-component-page/src/PageHeader.js
@@ -8,7 +8,7 @@ class PageHeader extends React.Component {
       <header className="cf-page__header">
         <h1 className="cf-page__title">{this.props.title}</h1>
         {this.props.subtitle &&
-          <p className="cf-page__subtitle">{this.props.subtitle}</p>}
+          <h2 className="cf-page__subtitle">{this.props.subtitle}</h2>}
       </header>
     );
   }
