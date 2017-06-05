698fc7bb8864390418eb222cdfd2861a6b0d7b27
tajo <vojtech@miksu.cz>
2017-06-05 16:57:50 -0700

    update snapshots and provider

--- packages/cf-style-provider/src/index.js
+++ packages/cf-style-provider/src/index.js
@@ -10,7 +10,6 @@ import prefixer from 'fela-plugin-prefixer';
 import fallbackValue from 'fela-plugin-fallback-value';
 import unit from 'fela-plugin-unit';
 import lvha from 'fela-plugin-lvha';
-import fontRenderer from 'fela-font-renderer';
 import validator from 'fela-plugin-validator';
 import beautifier from 'fela-beautifier';
 import monolithic from 'fela-monolithic';
@@ -20,15 +19,13 @@ import { ThemeProvider } from 'cf-style-container';
 
 const defaultOpts = {
   selectorPrefix: 'cf-',
-  dev: false,
-  fontNode: undefined,
-  cssNode: undefined
+  dev: false
 };
 
 export const createRenderer = opts => {
   const usedOpts = Object.assign({}, defaultOpts, opts);
   const plugins = [prefixer(), fallbackValue(), unit(), lvha()];
-  const enhancers = [fontRenderer(usedOpts.fontNode)];
+  const enhancers = [];
 
   if (usedOpts.dev === true) {
     plugins.push(validator());
@@ -46,19 +43,16 @@ export const createRenderer = opts => {
 export const StyleProvider = ({
   selectorPrefix,
   dev,
-  cssNode,
-  fontNode,
   children,
   ...restProps
 }) => {
   const renderer = createRenderer({
     selectorPrefix,
-    dev,
-    fontNode
+    dev
   });
   const child = Children.only(children);
   return (
-    <Provider renderer={renderer} mountNode={cssNode}>
+    <Provider renderer={renderer}>
       <ThemeProvider theme={variables}>
         {isValidElement(child) ? cloneElement(child, { ...restProps }) : child}
       </ThemeProvider>
@@ -69,8 +63,5 @@ export const StyleProvider = ({
 StyleProvider.defaultProps = defaultOpts;
 StyleProvider.propTypes = {
   dev: PropTypes.bool,
-  selectorPrefix: PropTypes.string,
-  cssNode: PropTypes.object,
-  fontNode: PropTypes.object,
-  children: PropTypes.node.isRequired
+  selectorPrefix: PropTypes.string
 };
