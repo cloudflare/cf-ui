080b15efc94a410a7fe7882679d09574d2a18807
Jimmy Yuen Ho Wong <wyuenho@users.noreply.github.com>
2017-06-21 17:17:42 +0100

    Styling utils improvements (#262)

--- packages/cf-style-container/test/index.js
+++ packages/cf-style-container/test/index.js
@@ -2,40 +2,51 @@ import React from 'react';
 import {
   createComponent,
   createComponentStyles,
-  applyTheme,
-  ThemeProvider
+  mergeThemes,
+  filterNone,
+  filterStyle,
+  mapChildren
 } from '../../cf-style-container/src/index';
+import { variables } from 'cf-style-const';
 import renderer from 'react-test-renderer';
-import { mount } from 'enzyme';
 import felaTestContext from '../../../felaTestContext';
 
-test('applyTheme should overwrite the baseTheme and update the context', () => {
-  const ThemedFoo = applyTheme(
-    createComponent(props => ({
-      color: props.theme.color
-    })),
-    () => ({
-      color: 'yellow'
-    })
-  );
-  const component = renderer.create(felaTestContext(<ThemedFoo />));
-  expect(component.toJSON()).toMatchSnapshot();
+test('mergesThemes should accept functions themes', () => {
+  const theme = mergeThemes(variables, () => ({
+    color: 'yellow'
+  }));
+  expect(theme).toMatchSnapshot();
 });
 
-test('applyTheme should overwrite the mainTheme and update the context', () => {
-  const ThemedFoo = applyTheme(
-    createComponent(props => ({
-      color: props.theme.color
-    })),
+test('mergesThemes should accept object themes', () => {
+  const theme = mergeThemes(variables, {
+    color: 'yellow'
+  });
+  expect(theme).toMatchSnapshot();
+});
+
+test('mergeThemes should override the baseTheme if more than one theme is provided', () => {
+  const theme = mergeThemes(
+    variables,
     () => ({
-      color: 'yellow'
+      color: 'yellow',
+      breakpoints: {
+        desktop: '1px',
+        tv: '2px'
+      }
     }),
-    () => ({
-      color: 'blue'
-    })
+    {
+      breakpoints: {
+        desktop: '3px'
+      }
+    }
   );
-  const component = renderer.create(felaTestContext(<ThemedFoo />));
-  expect(component.toJSON()).toMatchSnapshot();
+  expect(theme).toMatchSnapshot();
+});
+
+test('mergeThemes should return the baseTheme if no extra theme is provided', () => {
+  const theme = mergeThemes(variables);
+  expect(theme).toMatchSnapshot();
 });
 
 test('createComponent creates empty component', () => {
@@ -61,4 +72,22 @@ test('createComponentStyles creates a component', () => {
   );
   const component = renderer.create(felaTestContext(<FelaComponent />));
   expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('filterNone will filter out all keys with undefined or null values', () => {
+  expect(filterNone({ a: undefined, b: null, c: 1 })).toEqual({ c: 1 });
+});
+
+test('filterStyle will filter out the style key from an object', () => {
+  expect(filterStyle({ a: 1, b: 2, style: 3 })).toEqual({ a: 1, b: 2 });
+});
+
+test('mapChildren will call a function for each child, passing its index and the entire list of children as an array', () => {
+  const result = mapChildren([<div />], (child, i, children) => {
+    expect(child).toBeTruthy();
+    expect(i).toBeGreaterThanOrEqual(0);
+    expect(children.length).toBe(1);
+    return child;
+  });
+  expect(result).toHaveLength(1);
 });
