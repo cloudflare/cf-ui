d80d06d8ae8a1f78fa42b4dd3a84089fb9f9d6f4
tajo <vojtech@miksu.cz>
2017-06-23 11:46:45 -0700

    move the example into /packages so it is synced up

--- packages/example/source.js
+++ packages/example/source.js
@@ -4,7 +4,7 @@ import { StyleProvider } from 'cf-style-provider';
 import { Button } from 'cf-component-button';
 
 ReactDOM.render(
-  <StyleProvider cssNode={document.getElementById('stylesheet')}>
+  <StyleProvider>
     <Button type="primary" onClick={() => console.log('clicked')}>
       Primary Button
     </Button>
