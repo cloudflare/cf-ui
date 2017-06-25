2465eb28868e77b9d06403894a22607c54b2e094
Jimmy Yuen Ho Wong <jwong@cloudflare.com>
2017-06-26 00:17:48 +0100

    Format media query snapshots and upgrade polished because styled-components/polished#140

--- packages/cf-style-provider/test/index.js
+++ packages/cf-style-provider/test/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleProvider } from '../src/index';
+import { StyleProvider, felaSnapshot } from '../src/index';
 import { createComponent } from '../../cf-style-container/src/index';
 import { Button } from '../../cf-component-button/src/index';
 import renderer from 'react-test-renderer';
@@ -8,39 +8,49 @@ import { render } from 'react-dom';
 import { mount } from 'enzyme';
 import { html as beautify } from 'js-beautify';
 
-test('StyleProvider should render', () => {
-  const component = renderer.create(<StyleProvider><div /></StyleProvider>);
-  expect(component.toJSON()).toMatchSnapshot();
+describe('StyleProvider', () => {
+  it('should render', () => {
+    const component = renderer.create(<StyleProvider><div /></StyleProvider>);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  it('should render styles', () => {
+    const htmlNode = document.createElement('div');
+    document.body.appendChild(htmlNode);
+
+    const Foo = createComponent(() => ({
+      margin: '10px',
+      color: 'red',
+      desktop: {
+        fontSize: '1px'
+      }
+    }));
+
+    render(
+      <StyleProvider>
+        <Foo />
+      </StyleProvider>,
+      htmlNode
+    );
+
+    expect(
+      beautify(document.documentElement.outerHTML, { indent_size: 2 })
+    ).toMatchSnapshot();
+  });
+
+  it('should pass custom props through', () => {
+    const Foo = () => <div />;
+    const wrapper = mount(<StyleProvider number={10}><Foo /></StyleProvider>);
+    wrapper.setProps({ something: true });
+    expect(wrapper.prop('something')).toBeTruthy();
+    expect(wrapper.prop('number')).toBe(10);
+  });
 });
 
-test('StyleProvider should render styles', () => {
-  const htmlNode = document.createElement('div');
-  document.body.appendChild(htmlNode);
-
-  const Foo = createComponent(() => ({
-    margin: '10px',
-    color: 'red',
-    desktop: {
-      fontSize: '1px'
-    }
-  }));
-
-  render(
-    <StyleProvider>
-      <Foo />
-    </StyleProvider>,
-    htmlNode
-  );
-
-  expect(
-    beautify(document.documentElement.outerHTML, { indent_size: 2 })
-  ).toMatchSnapshot();
-});
-
-test('StyleProvider should pass custom props through', () => {
-  const Foo = () => <div />;
-  const wrapper = mount(<StyleProvider number={10}><Foo /></StyleProvider>);
-  wrapper.setProps({ something: true });
-  expect(wrapper.prop('something')).toBeTruthy();
-  expect(wrapper.prop('number')).toBe(10);
+describe('felaSnapshot', () => {
+  it('should return a formatted snapshot object', () => {
+    const Div = createComponent(() => ({ color: 'black' }));
+    const snapshot = felaSnapshot(<Div />);
+    expect(snapshot).toMatchSnapshot();
+  });
 });
