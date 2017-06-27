e6cb06133b4850eb57812867286dbfeacc8ded49
tajo <vojtech@miksu.cz>
2017-06-26 17:09:58 -0700

    fix(cf-style-provider): remove className prefixes and fix snapshot formatting

--- packages/cf-style-provider/src/felaSnapshot.js
+++ packages/cf-style-provider/src/felaSnapshot.js
@@ -1,20 +1,29 @@
 import renderer from 'react-test-renderer';
-import { html as beautify } from 'js-beautify';
-import CleanCSS from 'clean-css';
+import perfectionist from 'perfectionist';
 import felaTestContext from './felaTestContext';
 import createRenderer from './createRenderer';
-
-const cssCleaner = new CleanCSS({ format: 'beautify', level: 0 });
 
 export default component => {
   const felaRenderer = createRenderer({
     dev: true
   });
 
+  const prettifyFelaString = str =>
+    str
+      // adds extra lines before @media
+      .replace(/\@media/g, '\n\n@media')
+      // adds extra lines between keyframes and following selectors
+      .replace(/\}\.([a-zA-Z])/g, '}\n\n.$1')
+      // adds extra indentation and lines to nested selectors
+      .replace(
+        /\)\{(\.[\s\S]+?\})(\})/g,
+        (match, p1) => `) {\n  ${p1.replace(/\n/g, '\n  ')}\n}`
+      );
+
   return {
     component: renderer
       .create(felaTestContext(component, felaRenderer))
       .toJSON(),
-    styles: cssCleaner.minify(felaRenderer.renderToString()).styles
+    styles: `\n${prettifyFelaString(felaRenderer.renderToString())}\n`
   };
 };
