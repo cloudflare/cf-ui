55302100998b994e8d5716279e22132d91af0205
Jimmy Yuen Ho Wong <wyuenho@users.noreply.github.com>
2017-07-05 22:22:18 +0100

    fix(saveFile): Use blob to trigger a file download to get around 64k … (#297)
    * fix(saveFile): Use blob to trigger a file download to get around 64k attribute size limit
    
    * feat(saveFile): early warning for missing filename and file

--- packages/cf-util-http-file/src/saveFile.js
+++ packages/cf-util-http-file/src/saveFile.js
@@ -1,22 +1,15 @@
 export default function saveFile(filename, file, mimeType) {
-  mimeType = mimeType || 'data:application/octet-stream';
-  // Data URIs cannot be used for navigation, for scripting, or to
-  // populate frame or iframe elements in IE. Instead for IE specifically,
-  // use `msSaveBlob` to save the file.
+  if (!filename || !file) throw new Error('filename and file are required');
+  mimeType = mimeType || 'application/octet-stream';
   if ('msSaveBlob' in global.navigator) {
     global.navigator.msSaveBlob(new Blob([file]), filename);
     return;
   }
 
-  // Create a new anchor tag, set its href attribute to be a data url
-  // comprised of the contents of the response (aka our file), then click
-  // the link. This will prompt the browser to download the file.
+  const fileBlob = new Blob([file], { type: mimeType });
   const link = global.document.createElement('a');
-  link.href = mimeType + ',' + global.encodeURI(file);
-  if (filename) link.download = filename;
+  link.href = global.URL.createObjectURL(fileBlob);
+  link.download = filename;
 
-  // Dispatch fake click event to download the file
-  const event = global.document.createEvent('MouseEvents');
-  event.initEvent('click', true, true);
-  link.dispatchEvent(event);
+  link.click();
 }
