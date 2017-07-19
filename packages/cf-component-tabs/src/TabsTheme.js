2678dbc03d78e9ccd9a3e3ab3344331e9f85297d
Kristján Oddsson <kristjan@cloudflare.com>
2017-07-19 14:01:51 +0100

    fix(cf-component-tabs): set the correct colors
    In #314 we errounisly made assumptions where colors are kept in the
    theme. Trying to access the white color at `theme.color.white` and the
    smoke color at `theme.color.smoke` resulted in `undefined` values that
    `fela` swallows up and not emitting a rule.
    
    This patch references the colors in the correct location as well as
    minor housekeeping to keep the tabs theme a bit tidier.

--- packages/cf-component-tabs/src/TabsTheme.js
+++ packages/cf-component-tabs/src/TabsTheme.js
@@ -1,44 +1,46 @@
 export default baseTheme => ({
-  flex: 1,
-  background: baseTheme.color.white,
-  color: baseTheme.colorBlue,
-  cursor: 'pointer',
-  display: 'table-cell',
-  margin: 0,
-  padding: '1.5rem 1rem',
-  position: 'relative',
-  textAlign: 'center',
-  verticalAlign: 'middle',
-  outline: 'none',
-  userSelect: 'none',
+  item: {
+    flex: 1,
+    background: baseTheme.colorWhite,
+    color: baseTheme.colorBlue,
+    cursor: 'pointer',
+    display: 'table-cell',
+    margin: 0,
+    padding: '1.5rem 1rem',
+    position: 'relative',
+    textAlign: 'center',
+    verticalAlign: 'middle',
+    outline: 'none',
+    userSelect: 'none',
 
-  borderTopWidth: 0,
-  borderTopWidthSelected: 2,
-  borderLeftWidth: 0,
-  borderBottomWidth: 1,
-  borderBottomWidthSelected: 0,
-  borderRightWidth: 1,
+    borderTopWidth: 0,
+    borderTopWidthSelected: 2,
+    borderLeftWidth: 0,
+    borderBottomWidth: 1,
+    borderBottomWidthSelected: 0,
+    borderRightWidth: 1,
 
-  borderStyle: 'solid',
-  borderTopColor: baseTheme.color.smoke,
-  borderTopColorSelected: baseTheme.colorBlue,
-  borderLeftColor: baseTheme.color.smoke,
-  borderBottomColor: baseTheme.color.smoke,
-  borderRightColor: baseTheme.color.smoke,
+    borderStyle: 'solid',
+    borderTopColor: baseTheme.color.smoke,
+    borderTopColorSelected: baseTheme.colorBlue,
+    borderLeftColor: baseTheme.color.smoke,
+    borderBottomColor: baseTheme.color.smoke,
+    borderRightColor: baseTheme.color.smoke,
 
-  '&:last-child': {
-    borderRightWidth: 0
-  },
+    '&:last-child': {
+      borderRightWidth: 0
+    },
 
-  '&:focus': {
-    '&::after': {
-      outline: 'none'
+    '&:focus': {
+      '&::after': {
+        outline: 'none'
+      }
+    },
+
+    '&:hover': {
+      background: baseTheme.color.moonshine,
+      backgroundSelected: baseTheme.colorWhite,
+      color: baseTheme.colorBlue
     }
-  },
-
-  '&:hover': {
-    background: baseTheme.color.moonshine,
-    backgroundSelected: baseTheme.colorWhite,
-    color: baseTheme.colorBlue
   }
 });
