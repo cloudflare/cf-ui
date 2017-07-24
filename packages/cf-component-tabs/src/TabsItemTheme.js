ec86b1ec0a5bd6632b5d77743cc09aab319191d7
Kristján Oddsson <kristjan@cloudflare.com>
2017-07-24 14:59:35 +0100

    feat(cf-component-tabs): `TabsItem` own component
    In order to be able to apply styles to a "tab item" we need to abstract
    it out into it's own component. That can be optionally be passed down to
    the `Tabs` component defaulting to the styles previously applied.
    
    This change is backwards compatible. See
    `packages/cf-component-tabs/example/basic/component.js` for the optional
    component property.

---
+++ packages/cf-component-tabs/src/TabsItemTheme.js
@@ -0,0 +1,44 @@
+export default baseTheme => ({
+  flex: 1,
+  background: baseTheme.colorWhite,
+  color: baseTheme.colorBlue,
+  cursor: 'pointer',
+  display: 'table-cell',
+  margin: 0,
+  padding: '1.5rem 1rem',
+  position: 'relative',
+  textAlign: 'center',
+  verticalAlign: 'middle',
+  outline: 'none',
+  userSelect: 'none',
+
+  borderTopWidth: 0,
+  borderTopWidthSelected: 2,
+  borderLeftWidth: 0,
+  borderBottomWidth: 1,
+  borderBottomWidthSelected: 0,
+  borderRightWidth: 1,
+
+  borderStyle: 'solid',
+  borderTopColor: baseTheme.color.smoke,
+  borderTopColorSelected: baseTheme.colorBlue,
+  borderLeftColor: baseTheme.color.smoke,
+  borderBottomColor: baseTheme.color.smoke,
+  borderRightColor: baseTheme.color.smoke,
+
+  '&:last-child': {
+    borderRightWidth: 0
+  },
+
+  '&:focus': {
+    '&::after': {
+      outline: 'none'
+    }
+  },
+
+  '&:hover': {
+    background: baseTheme.color.moonshine,
+    backgroundSelected: baseTheme.colorWhite,
+    color: baseTheme.colorBlue
+  }
+});
--- packages/cf-component-tabs/src/index.js
+++ packages/cf-component-tabs/src/index.js
@@ -2,16 +2,22 @@ import { applyTheme } from 'cf-style-container';
 
 import TabsUnstyled from './Tabs';
 import TabsTheme from './TabsTheme';
+import TabsItemUnstyled from './TabsItem';
+import TabsItemTheme from './TabsItemTheme';
 import TabsPanelUnstyled from './TabsPanel';
 import TabsPanelTheme from './TabsPanelTheme';
 
 const Tabs = applyTheme(TabsUnstyled, TabsTheme);
+const TabsItem = applyTheme(TabsItemUnstyled, TabsItemTheme);
 const TabsPanel = applyTheme(TabsPanelUnstyled, TabsPanelTheme);
 
 export {
   Tabs,
   TabsUnstyled,
   TabsTheme,
+  TabsItem,
+  TabsItemUnstyled,
+  TabsItemTheme,
   TabsPanel,
   TabsPanelUnstyled,
   TabsPanelTheme
