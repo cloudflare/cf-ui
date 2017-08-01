9374888bb661c0868b39e992502e7ea9e1f9da52
Kristján Oddsson <kristjan@cloudflare.com>
2017-08-01 14:36:43 +0100

    feat(cf-component-radio): abstract children out of group
    BREAKING CHANGE: Changed the API that accepts radio options as props
    and rather accept the whole `<Radio />` components as children.  This
    allows us to pass in `<Radio />` components that have different
    themes or even behave differently.
    
    Before:
    
    ```jsx
    <RadioGroup
      value={this.state.radioValue}
      onChange={this.onRadioChange}
      options={[
        { label: 'Option 1', name: 'group-option-1', value: 'option1' },
        { label: 'Option 2', name: 'group-option-2', value: 'option2' }
      ]}
    />
    ```
    
    After:
    
    ```jsx
    <RadioGroup value={this.state.radioValue} onChange={this.onRadioChange}>
      <RadioGroupItem
        label="Option 1"
        name="group-option-1"
        value="option1"
      />
      <RadioGroupItem
        label="Option 2"
        name="group-option-2"
        value="option2"
      />
    </RadioGroup>
    ```
    
    Related-to: #

--- packages/cf-component-radio/src/RadioGroup.js
+++ packages/cf-component-radio/src/RadioGroup.js
@@ -1,40 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { createComponent, applyTheme } from 'cf-style-container';
+import { createComponent } from 'cf-style-container';
 
-import RadioUnstyled from './Radio';
-import RadioTheme from './RadioTheme';
-
-const Radio = applyTheme(RadioUnstyled, RadioTheme, () => ({
-  marginTop: '1em',
-
-  '&:first-child': {
-    marginTop: 0
-  }
-}));
-
-const styles = () => ({
-  textAlign: 'left',
-  display: 'inline-block',
-  verticalAlign: 'middle'
+const styles = ({ theme }) => ({
+  textAlign: theme.textAlign,
+  display: theme.display,
+  verticalAlign: theme.verticalAlign
 });
 
 class RadioGroup extends React.Component {
   render() {
     return (
       <div className={this.props.className}>
-        {this.props.options.map(option => {
-          return (
-            <Radio
-              key={option.name}
-              label={option.label}
-              name={option.name}
-              value={option.value}
-              checked={this.props.value === option.value}
-              onChange={this.props.onChange}
-            />
-          );
-        })}
+        {React.Children.map(this.props.children, Radio =>
+          React.cloneElement(Radio, {
+            key: Radio.props.name,
+            checked: this.props.value === Radio.props.value,
+            onChange: this.props.onChange
+          })
+        )}
       </div>
     );
   }
